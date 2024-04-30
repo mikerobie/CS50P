1d00ab6f3718e9ebf621d1fa5ee7e4948934b543
Mike Robie <insightfulcopywriter@gmail.com>
2024-04-30 16:44:21 -0500

    4.30 updates

--- conditionals.py
+++ conditionals.py
@@ -16,8 +16,8 @@
 
 #////////////////
 #Lowercases string before comparing
-answer = input("Do you agree?").strip().lower()
-if answer == "yes":
-    print("Agreed")
-else:
-    print("Not agreed")
+#answer = input("Do you agree?").strip().lower()
+#if answer == "yes":
+ #   print("Agreed")
+#else:
+#    print("Not agreed")

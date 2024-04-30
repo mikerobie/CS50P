b412b1db36803f9c998ad5fbbff1d5e5cbfc3d75
Mike Robie <insightfulcopywriter@gmail.com>
2024-04-29 22:21:26 -0500

    agree not agree

--- conditionals.py
+++ conditionals.py
@@ -1,2 +1,14 @@
-a = "Hello"
-print(a)
+# Compares strings
+
+#answer = input('Do you agree?')
+#if answer == "yes":
+#    print('Agreed')
+#else:
+#    print("Not agreed")
+
+# Strips string before comparing
+answer = input("Do you agree?").strip()
+if answer == "yes":
+    print("Agreed")
+else:
+    print("Not agreed")

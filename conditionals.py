4cf1f710ee8a20229f1e0a9a880aea2cd870f074
Mike Robie <insightfulcopywriter@gmail.com>
2024-04-29 22:46:44 -0500

    Agree/Disagree with .strip and .lower

--- conditionals.py
+++ conditionals.py
@@ -6,8 +6,17 @@
 #else:
 #    print("Not agreed")
 
+#//////////////////////////////
 # Strips string before comparing
-answer = input("Do you agree?").strip()
+#answer = input("Do you agree?").strip()
+#if answer == "yes":
+#    print("Agreed")
+#else:
+#    print("Not agreed")
+
+#////////////////
+#Lowercases string before comparing
+answer = input("Do you agree?").strip().lower()
 if answer == "yes":
     print("Agreed")
 else:

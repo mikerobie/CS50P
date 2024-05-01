6f5b13bcdea989bdb5e897ad6f472130ec93d819
Mike Robie <insightfulcopywriter@gmail.com>
2024-05-01 15:06:31 -0500

    5.1 update

---
+++ compare4.py
@@ -0,0 +1,11 @@
+#Demonstrates equality
+
+x = int(input("What is x ?"))
+
+y = int(input("What is y ?"))
+
+if x == y:
+    print("X is equal to y")
+
+else:
+    print("X is not equal to y")
---
+++ grade0.py
@@ -0,0 +1,9 @@
+#Demonstrates inequalities and logical operators
+
+score = int(input("Score: "))
+
+if score >= 90 and score <= 100:
+    print("Grade: A")
+elif score >= 80 and score <= 90:
+    print("Grade: B")
+    

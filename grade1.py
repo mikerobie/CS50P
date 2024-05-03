9ee749ee5b16aa6906d845c03e5098b9315af16d
Mike Robie <insightfulcopywriter@gmail.com>
2024-05-03 12:49:42 -0500

    5.3 grade1.py finished

--- grade1.py
+++ grade1.py
@@ -6,4 +6,9 @@ if 90 <= score and score <= 100:
     print("Grade: A")
 elif 80 <= score and score <= 90:
     print("Grade: B")
- 
+elif 70 <= score and score <= 80:
+    print("Grade: C")
+elif 60 <= score and score <= 70:
+    print("Grade: D")
+else:
+    print("Grade: F")

fac8e7069092e4dcac7eea8f7c86d1c00fd2cb6f
Mike Robie <insightfulcopywriter@gmail.com>
2024-05-05 14:24:31 -0500

    5.5 house.py notes

--- house.py
+++ house.py
@@ -4,5 +4,9 @@ name = input("What's your name?")
 
 if name == "Harry" or name == "Hermione" or name == "Ron":
     print("Gryffindor")
+elif name == "Draco":
+    print ("Slytherin")
+else:
+    print("Who?")
 
     

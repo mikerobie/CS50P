c8159ca4d3d4b8eee9886e44c8fc4e1d8acb0c22
Mike Robie <insightfulcopywriter@gmail.com>
2024-05-11 08:22:32 -0500

    5.11.24 parity1.py

--- parity0.py
+++ parity0.py
@@ -4,3 +4,5 @@ x = int(input("What is x? "))
 
 if x % 2 == 0:
     print("Even")
+else:
+    print("Odd")
---
+++ parity1.py
@@ -0,0 +1,16 @@
+#Demonstrates a function that returns a bool
+
+def main():
+    x = int(input("What is x?"))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
